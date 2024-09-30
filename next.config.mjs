09ea680c03fbb1c8311536e2a666e62e056e44e5
brijesh <brijesh.panchal@nstacksoftech.com>
2024-09-30 15:17:02 +0530

    remove unsafe inline

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,30 @@
 /** @type {import('next').NextConfig} */
+const clientName = process?.env?.NEXT_PUBLIC_CLIENT_NAME || 'qa';
 
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: false,
   async headers() {
+    const isProduction = process.env.NODE_ENV === 'production';
+
+    const policies = {
+      'default-src': ["'self'"],
+      'script-src': ["'self'", process.env.NEXT_PUBLIC_SCRIPT_SRC_DOMAINS],
+      'style-src': ["'self'", "'unsafe-inline'", process.env.NEXT_PUBLIC_STYLE_SRC_DOMAINS],
+      'img-src': ["'self'", 'data:', '*'],
+      'font-src': ["'self'"],
+      'frame-src': ["'self'", process.env.NEXT_PUBLIC_FRAME_SRC_DOMAINS],
+      'connect-src': ["'self'", process.env.NEXT_PUBLIC_CONNECT_SRC_DOMAINS],
+    };
+
+    if (!isProduction) {
+      policies['script-src'].push("'unsafe-eval'");
+    }
+
+    const contentSecurityPolicy = Object.entries(policies)
+      .map(([key, values]) => `${key} ${values.join(' ')}`)
+      .join('; ');
+
     return [
       {
         source: '/(.*)',
@@ -15,17 +36,7 @@ const nextConfig = {
           { key: 'X-Content-Type-Options', value: 'nosniff' },
           {
             key: 'Content-Security-Policy',
-            value: `
-                    default-src 'self';  
-                    frame-src 'self' ${process.env.NEXT_PUBLIC_FRAME_SRC_DOMAINS};
-                    script-src 'self' 'unsafe-inline' ${process.env.NODE_ENV === 'production' ? '' : 'unsafe-eval'} ${process.env.NEXT_PUBLIC_SCRIPT_SRC_DOMAINS};
-                    connect-src 'self' ${process.env.NEXT_PUBLIC_CONNECT_SRC_DOMAINS};
-                    style-src 'self' 'unsafe-inline' ${process.env.NEXT_PUBLIC_STYLE_SRC_DOMAINS};  
-                    img-src 'self' data: *;
-                    font-src 'self';
-                    `
-              .trim()
-              .replace(/\s+/g, ' '),
+            value: contentSecurityPolicy,
           },
           {
             key: 'X-Frame-Options',
@@ -37,7 +48,29 @@ const nextConfig = {
           },
         ],
       },
+      {
+        source: '/.well-known/apple-app-site-association',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/json',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=86400',
+          },
+        ],
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/.well-known/apple-app-site-association',
+        destination: `/${clientName}/apple-app-site-association`,
+      },
     ];
   },
 };
+
 export default nextConfig;
