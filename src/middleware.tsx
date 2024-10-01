c008cdef2c42ef2a725dde2eb54d3b34635313be
brijesh <brijesh.panchal@nstacksoftech.com>
2024-10-01 14:54:35 +0530

    add nonce in middleware

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,32 +1,51 @@
-import { NextResponse, NextRequest } from 'next/server';
-import { LOCALE, nl } from '@/utils/constants';
-import { allowedLocales } from '@/utils/constants';
-import { destructurePathValues, generateAdditionalPath } from './utils/helpers';
+import { NextRequest, NextResponse } from 'next/server';
 
-const rootRoute = ['/'];
-const publicRoutes = ['/login', '/register', '/verifyCode', '/verifyEmail'];
+export function middleware(request: NextRequest) {
+  const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
+  const isProduction = process.env.NODE_ENV === 'production';
 
-export function middleware(req: NextRequest) {
-  const token = req.cookies.get('token')?.value;
-  const path = req.nextUrl.pathname;  
-  const { searchParams } = req.nextUrl;
-  const { getwpmValue, getYearValue, getMonthValue } = destructurePathValues(searchParams);
+  const policies = {
+    'default-src': ["'self'"],
+    'script-src': [`'self'`, `'nonce-${nonce}'`, process.env.NEXT_PUBLIC_SCRIPT_SRC_DOMAINS],
+    'style-src': [`'self'`, `'nonce-${nonce}'`, process.env.NEXT_PUBLIC_STYLE_SRC_DOMAINS],
+    'frame-src': ["'self'", process.env.NEXT_PUBLIC_FRAME_SRC_DOMAINS],
+    'connect-src': ["'self'", process.env.NEXT_PUBLIC_CONNECT_SRC_DOMAINS],
+    'img-src': ["'self'", 'data:', '*'],
+    'font-src': ["'self'"],
+    'frame-ancestors': ["'none'"],
+    'object-src': ["'none'"],
+    'base-uri': ["'self'"],
+  };
 
+  if (!isProduction) {
+    policies['script-src'].push("'unsafe-eval'");
+  }
 
+  const contentSecurityPolicy = Object.entries(policies)
+    .map(([key, values]) => `${key} ${values.join(' ')}`)
+    .join('; ');
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set('x-nonce', nonce);
+
+  requestHeaders.set('Content-Security-Policy', contentSecurityPolicy);
+
+  const response = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
+  response.headers.set('Content-Security-Policy', contentSecurityPolicy);
+
+  return response;
 }
-
 export const config = {
   matcher: [
-    '/',
-    '/login',
-    '/register',
-    '/verifyEmail',
-    '/:locale/dashboard',
-    '/:locale/dashboard/my-vehicles',
-    '/:locale/dashboard/my-address',
+    {
+      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+      missing: [
+        { type: 'header', key: 'next-router-prefetch' },
+        { type: 'header', key: 'purpose', value: 'prefetch' },
+      ],
+    },
   ],
 };
-// const middlewareConfig = {
-//   locales: [en, nl],
-//   defaultLocale: DEFAULT_LOCALE,
-// };
